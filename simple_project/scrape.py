3e84c9d06f21bd8cf9d4bdc85565458a099efb97
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-08 19:55:39 +0300

    ok

--- simple_project/scrape.py
+++ simple_project/scrape.py
@@ -1,8 +1,5 @@
 import requests
-
 ulr="https://secure-retreat-92358.herokuapp.com/"
 value={"fname":"Rafia","lname":"Kedir","email":"yuanizaaa@gmail.com"}
-
 resp=requests.post(url=ulr,data=value)
-
 print(resp.content)
