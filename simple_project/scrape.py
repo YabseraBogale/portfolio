bec635eef18cefb4d47a647d2a603062085dab1d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-15 19:29:03 +0300

    ok

--- simple_project/scrape.py
+++
@@ -1,5 +0,0 @@
-import requests
-ulr="https://secure-retreat-92358.herokuapp.com/"
-value={"fname":"Rafia","lname":"Kedir","email":"yuanizaaa@gmail.com"}
-resp=requests.post(url=ulr,data=value)
-print(resp.content)
