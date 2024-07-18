81b8e1be8c981b51ff91a593048d2ef0c6b53801
YabseraBogale <yabseraogalercd30932013@gmail.com>
2024-07-18 18:14:58 +0300

    ok

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,url_for,request,redirect
 from database import Database
-# # wtf is wrong with github
+# # wtf is wrong with github deleteing doesn't count
 app=Flask(__name__)
 database=Database()
 
