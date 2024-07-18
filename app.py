02708904053a0703643278825f984cc89c827641
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-18 17:09:17 +0300

    ok

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask,render_template,url_for,request,redirect
 from database import Database
-
+# # wtf is wrong with github
 app=Flask(__name__)
-
+database=Database()
 
 @app.route("/")
 def home():
