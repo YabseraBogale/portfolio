412b2011bf3378a1eb52c9c7389a97a1eb23ae96
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-22 18:03:30 +0300

    ok

---
+++ Wiki-App/wikiapp.js
@@ -0,0 +1,3 @@
+const api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch='
+const btn=document.getElementById("article")
+const uri=api+btn.value 
