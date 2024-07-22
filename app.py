6ca26b5f3aabf0b0d1fac323838a276ff24e685a
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-22 18:11:13 +0300

    ok

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ database=Database()
 @app.route("/")
 def home():
     return render_template("home.html")
-
+@app.route("/weather")
+def weather():
+    return render_template("weather.html")
 
 if __name__=="__main__":
     app.run(debug=True)
