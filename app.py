9ae0a06d2a08e271e9991366a12ff70612b54fb9
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-22 18:18:47 +0300

    ok

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask,render_template,url_for,request,redirect
-from database import Database
-# # wtf is wrong with github deleteing doesn't count
+
 app=Flask(__name__)
-database=Database()
+
 
 @app.route("/")
 def home():
