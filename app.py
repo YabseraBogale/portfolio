6223f7d11a403658b3f431edef1c751fbc75e717
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-22 18:09:41 +0300

    ok

--- app.py
+++ app.py
@@ -8,27 +8,6 @@ database=Database()
 def home():
     return render_template("home.html")
 
-@app.route("/project")
-def project():
-    return render_template("project.html")
-
-@app.route("/<name>")
-def urishort(name):
-    data=database.GetSpecificOldUri(str(name))
-    return redirect(data,302)
-
-@app.route("/project/urishortener",methods=['GET','POST'])
-def urishorten():
-    if request.method=="POST":
-        data=database.InsertUriShortener(request.form["uri"])
-        data=database.GetSpecificNewUri(request.form["uri"])
-        return render_template("urishortener.html",newuri=data[0],olduri=request.form["uri"]) 
-    return render_template("urishortener.html")
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
 
 if __name__=="__main__":
     app.run(debug=True)
