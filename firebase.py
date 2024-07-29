fe90f224ef37b76925f68cfdca179502980b7f28
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-29 11:25:55 +0300

    ok

--- firebase.py
+++ firebase.py
@@ -2,5 +2,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("./config.json")
+cred = credentials.Certificate("./admin_secret.json")
 firebase_admin.initialize_app(cred)
