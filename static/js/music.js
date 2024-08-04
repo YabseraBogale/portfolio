5691d84e205f0c800acfe4997ae92003941fa0e7
YabseraBogale <yabserapython@gmail.com>
2024-08-04 13:51:56 +0300

    ok

--- static/js/music.js
+++ static/js/music.js
@@ -1,7 +1,12 @@
 fetch("http://127.0.0.1:5000/music/list")
     .then(response=>response.json())
     .then(data=>{
-        console.log(data)
+        let music=""
+       let order=document.getElementsByTagName("ol")
+       for(let i=0;i<data.length;i++){
+        music+="<li>"+data[i]+"</li>"
+       }
+       order[0].innerHTML=music
     }).catch(err=>{
         console.error(err)
     })                                  
