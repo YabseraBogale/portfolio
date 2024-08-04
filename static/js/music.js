968bdaaa87bb12973d341bbdfc367c14978e3f50
YabseraBogale <yabserapython@gmail.com>
2024-08-04 14:32:24 +0300

    ok

--- static/js/music.js
+++ static/js/music.js
@@ -1,12 +1,22 @@
 fetch("http://127.0.0.1:5000/music/list")
     .then(response=>response.json())
     .then(data=>{
-        let music=""
+       let place="{{url_for('static',filename='music/8_Muse-Hysteria.mp3')}}"
+       let music=""
+       let audio=document.getElementsByTagName("audio")
        let order=document.getElementsByTagName("ol")
        for(let i=0;i<data.length;i++){
-        music+="<li>"+data[i]+"</li>"
+        music+="<li>"+data[i].split(".m")[0]+"</li>"
        }
        order[0].innerHTML=music
+       let list=document.getElementsByTagName("li")
+       for(let i=0;i<list.length;i++){
+            list[i].addEventListener("click",function(){
+                
+                
+            })
+       }
+       
     }).catch(err=>{
         console.error(err)
     })                                  
